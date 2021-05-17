9c205434a4bac92bb142555abf819411419a7204
pksingh786 <56902261+pksingh786@users.noreply.github.com>
2021-05-17 19:50:30 +0530

    Add files via upload

---
+++ BASIC CALCULATOR.py
@@ -0,0 +1,10 @@
+#addition
+first_num=10
+second_num=20
+sum=first_num+second_num
+print("the sum is:",sum)
+#subtraction
+first_num=45
+second_num=20
+sub=first_num-second_num
+print("the sub is:",sub)
